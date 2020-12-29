043bf68b68f7eee388eb40d31fe2f52bee947920
Jiten1312 <jitendhimmar@gmail.com>
2020-12-29 13:46:03 +0530

    firstcommit

---
+++ administrator/logs/error.php
@@ -0,0 +1,9 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2020-12-27 13:58:02 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2020-12-27T13:58:01+00:00	INFO ::1	joomlafailure	Empty password not allowed.
+2020-12-27T14:00:56+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2020-12-27T14:01:01+00:00	INFO ::1	joomlafailure	Empty password not allowed.
